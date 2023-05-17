86538550908b9c9f22896bba1faa99f6636295b5
Harshadkumar Patel <64983530+Harshpateloo7@users.noreply.github.com>
2023-05-16 22:20:00 -0400

    Minor update

--- Backend/app.js
+++ Backend/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
+const { connectDB } = require("./config/db.config");
 
 // Set body-parser
 app.use(bodyParser.urlencoded({ extended: true }))
@@ -8,17 +9,24 @@ app.use(bodyParser.json())
 
 const port = process.env.PORT || 3000;
 
-// app.use('/', (req, res) => {
-//     res.json({ message: 'Hello world!'})
-// })
+// Connect Database
+connectDB();
+
+
+app.get('/', (req, res) => {
+    res.json({ message: 'Hello world!'})
+})
 
 // Error handler
 const handleError = require('./utils/errorHandler');
 
-app.use("*", (req, res) => {
-    console.log(name);
-    // const error = new Error('Route not found');
-    // error.status = 404;
+app.use((req, res, next) => {
+    const error = new Error("Not Found")
+    error.status = 404;
+    next(error)
+})
+
+app.use((error, req, res, next) => {
     handleError(error, res);
 })
 
