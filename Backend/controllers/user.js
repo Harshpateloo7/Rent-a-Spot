d3e1ac300114d10dfcc4debe44a07b71d6672555
Harshadkumar Patel <64983530+Harshpateloo7@users.noreply.github.com>
2023-06-05 15:00:59 -0400

    Update

--- Backend/controllers/user.js
+++ Backend/controllers/user.js
@@ -50,7 +50,7 @@ userRouter.post("/register", async (req, res) => {
                 // Password encryption
                 password = bcrypt.hashSync(password, 10);
                 const user = await User.create({ name, email, password, type });
-                res.json(user);
+                res.status(201).json(user);
             }
         }
     } catch (error) {
